b2329e486d0426c4099262971fcbea4e7a3aa5ac
fereydoon salemi <afereydoon.s@gmail.com>
2020-09-06 15:19:02 +0430

    ..

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/','HomeController@index');
+//Route::get('/','HomeController@index');
 
 Route::resource('post','PostController');
-
-
-
-
-
 
 Auth::routes();
 
