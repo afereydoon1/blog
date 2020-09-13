4040861d953fd002a9272b77b739e178d2d3c7cc
fereydoon salemi <afereydoon.s@gmail.com>
2020-09-13 17:38:59 +0430

    bazyabi project

---
+++ app/Http/Controllers/CommntController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\commnt;
+use Illuminate\Http\Request;
+
+class CommntController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\commnt  $commnt
+     * @return \Illuminate\Http\Response
+     */
+    public function show(commnt $commnt)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\commnt  $commnt
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(commnt $commnt)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\commnt  $commnt
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, commnt $commnt)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\commnt  $commnt
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(commnt $commnt)
+    {
+        //
+    }
+}
